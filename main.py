f1fe4bb7a18fe928572c5837c8b34728c9833a37
rutraq <a3332133@gmail.com>
2020-09-11 14:01:59 +0300

    firebase api

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from PyQt5 import Qt, QtGui, QtCore
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QCheckBox
 import mainform
-
+from firebase import Firebase
+from Crypto.PyblicKey import RSA
 
 class MainForm(QtWidgets.QMainWindow, mainform.Ui_MainWindow):
     def __init__(self):
@@ -11,12 +12,22 @@ class MainForm(QtWidgets.QMainWindow, mainform.Ui_MainWindow):
         self.shops = ["nike", "хуяйк", "женина залупа", "бабкин хуй"]
         self.items = ["jacket", "sneakers", "shirt", "jeans", "t-shirts"]
         self.add_values()
+        self.firebase_check()
 
     def add_values(self):
         for shop in self.shops:
             self.comboBoxShop.addItem(shop)
         for item in self.items:
             self.comboBoxItem.addItem(item)
+
+    def firebase_check(self):
+        config = {
+            "apiKey": "AIzaSyAl0sJD8bl30gnWH9juh6pywzOzsuzSdso",
+            "authDomain": "wear-by-weather.firebaseapp.com",
+            "databaseURL": "https://wear-by-weather.firebaseio.com",
+            "storageBucket": "wear-by-weather.appspot.com",
+        }
+        firebase2 = Firebase(config)
 
 
 if __name__ == "__main__":
