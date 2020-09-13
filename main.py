dbaef56a703069651927ccf8975a5174c7a8167f
rutraq <a3332133@gmail.com>
2020-09-13 22:27:54 +0300

    firestore read and write

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class MainForm(QtWidgets.QMainWindow, mainform.Ui_MainWindow):
         self.styles = ["Casual", "Sport", "Official"]
         self.shops = {}
         self.url = {}
+        self.unique_number = int
         self.read_data()
         self.add_values()
         self.pushButton.pressed.connect(self.write_data)
@@ -44,29 +45,50 @@ class MainForm(QtWidgets.QMainWindow, mainform.Ui_MainWindow):
 
     def write_data(self):
         db = firestore.client()
-        self.shops[str(len(self.shops) + 1)] = self.lineEditPath.text()
-        doc_ref = db.collection('shops').document('shops')
-        doc_ref.set(self.shops)
-        self.comboBoxShop.clear()
-        doc_ref = db.collection("Links").document("links")
+        self.url[str(self.unique_number)] = self.lineEditUrl.text()
+        doc_ref = db.collection('Links').document('links')
         doc_ref.set(self.url)
-        self.add_values()
+        shop_number = int
+        for k, v in self.shops.items():
+            if v == self.comboBoxShop.currentText():
+                shop_number = k
+        collection = "{0}-{1}-{2}".format(self.comboBoxGender.currentText(), self.comboBoxWeight.currentText(),
+                                          self.comboBoxStyle.currentText())
+        items = self.document_read(collection, shop_number)
+        items[str(self.unique_number)] = "{0}{1}{2}".format(self.unique_number, self.comboBoxItem.currentText(),
+                                                            shop_number)
+        doc_ref = db.collection(collection).document(self.lineEditStyleNumber.text())
+        doc_ref.set(items)
+
+        self.unique_number += 1
 
     def read_data(self):
         db = firestore.client()
-        shops = db.collection("shops")
-        docs = shops.stream()
-        for doc in docs:
-            self.shops = doc.to_dict()
-        urls = db.collection("Links")
-        docs = urls.stream()
-        for doc in docs:
-            self.url = doc.to_dict()
+        self.shops = self.standard_read("shops")
+        self.url = self.standard_read("Links")
+        self.unique_number = int(self.standard_read("UniqueNumber")['1'])
 
-        doc_ref = db.collection('Casual').document('2')
+        doc_ref = db.collection('Men-NormalBodyWeight-Casual').document('1')
         doc = doc_ref.get()
         if doc.exists:
             print(doc.to_dict())
+
+    @staticmethod
+    def standard_read(collection):
+        db = firestore.client()
+        info = db.collection(collection)
+        docs = info.stream()
+        for doc in docs:
+            info = doc.to_dict()
+        return info
+
+    @staticmethod
+    def document_read(collection, number):
+        db = firestore.client()
+        doc_ref = db.collection(collection).document(str(number))
+        doc = doc_ref.get()
+        if doc.exists:
+            return doc.to_dict()
 
 
 if __name__ == "__main__":
